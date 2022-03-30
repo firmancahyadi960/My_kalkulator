65a2fe19d677f4fcadf1ac18880c6c8b5ee73b6a
firman <firmancahyadi960@gmail.com>
2022-03-30 14:40:57 +0800

    first commit

---
+++ script.js
@@ -0,0 +1,84 @@
+let prevNumber = '';
+let calculationOperator = '';
+let currentNumber = '0';
+
+const displayScreen = document.querySelector('.display');
+
+const updateScreen = (number) => {
+    displayScreen.value = number;
+}
+
+const inputNumber = (number) => {
+    if (currentNumber === '0'){
+        currentNumber = number;
+    }else {
+        currentNumber += number;
+    }
+}
+
+const numbers = document.querySelectorAll("#number");
+
+numbers.forEach((number) => {
+    number.addEventListener("click", (event) => {
+        inputNumber(event.target.value);
+        updateScreen(currentNumber);
+    });
+});
+
+const operators = document.querySelectorAll("#operator");
+
+operators.forEach((operator) => {
+    operator.addEventListener("click", (event) => {
+        inputOperator(event.target.value);
+    });
+});
+
+const inputOperator = (operator) => {
+    prevNumber = currentNumber;
+    calculationOperator = operator;
+    currentNumber = '';
+}
+
+const equalSign = document.querySelector('#equal');
+
+equalSign.addEventListener('click', () => {
+    calculate();
+    updateScreen(currentNumber);
+});
+
+const calculate = () => {
+    let result = '';
+    switch(calculationOperator) {
+        case '+':
+            result = parseInt(prevNumber) + parseInt(currentNumber);
+            break;
+        case '-':
+            result = parseInt(prevNumber) -  parseInt(currentNumber);
+            break;
+        case '*':
+            result = parseInt(prevNumber) *  parseInt(currentNumber);
+            break;
+        case '/':
+            result = parseInt(prevNumber) /  parseInt(currentNumber);
+            break;
+        case '%':
+            result = parseInt(prevNumber) %  parseInt(currentNumber);
+            break;
+        default:
+            break;
+    }
+    currentNumber = result;
+    calculationOperator = '';
+}
+
+const clearBtn = document.querySelector('#all-clear');
+clearBtn.addEventListener('click',() => {
+    clearAll();
+    updateScreen(currentNumber);
+});
+
+const clearAll = () => {
+    prevNumber = '';
+    calculationOperator = '';
+    currentNumber = '0';
+}
